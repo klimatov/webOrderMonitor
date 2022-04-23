26c563b2179a438ed8fd08fb3a0d828f5cc8cfa2
klimatov <klimatov@mail.ru>
2022-04-23 20:50:26 +0700

    v 0.1 + служба опроса вебок каждые 30 секунд, после опроса + обработчик результата -> появились новые? исчезла старая? список выведенных и опрошенный не отличаются? + появились новые? сохраняем данные в список + вывод в чат, добавляем messageId в данных + исчезла старая? меняем текст, добавив ПОДТВЕРЖДЕНА по message Id + исчезла старая? удаляем вебку из данных в списке + наименование товара обернуть в ссылку + оформление с emoji + Получение данных по остаткам и добавление их в пару к итемсам + Вывод остатков в сообщение + убрать отображение превью по ссылке + переделать опрос на /list-simple каждые 30 секунд + добавить Подтверждена болдом вверху

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,9 @@
 import bot.TGbot
-import dev.inmo.tgbotapi.types.ChatId
-import orderProcessing.BotMessage
 import orderProcessing.OrderDaemon
-import orderProcessing.data.*
 
 suspend fun main() {
 
     TGbot.botDaemonStart()
     OrderDaemon.orderDaemonStart()
 
-//    while (true) {}
-//myTest()
 }
