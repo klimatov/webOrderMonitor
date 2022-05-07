f8db72a4e09fd38f7c371aa32cb08b489a10ddd8
klimatov <klimatov@mail.ru>
2022-05-07 13:39:23 +0700

    v 0.8 + автозапуск при включении/загрузке устройства (проверено на API 25)

---
+++ app/src/main/java/com/github/klimatov/webordermonitor/BootCompletedReceiver.kt
@@ -0,0 +1,17 @@
+package com.github.klimatov.webordermonitor
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            Log.i("webOrderMonitor", "ACTION_BOOT_COMPLETED is recived and app is launched ")
+            val autoStartIntent = Intent(context, MainActivity::class.java)
+            autoStartIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(autoStartIntent)
+        }
+    }
+}
